c2e1cb588b4ef69052a4ac0b15a9660e8655ca5f
Marlon Raskin <marlonjraskin@gmail.com>
2019-07-10 16:18:23 -0400

    Stroyboard and outlets are setup. Enum is set and convert function is finished (not implemented yet). Selecting between different currencies works.

--- Currency-Converter/CurrencyConverterViewController.swift
+++ Currency-Converter/CurrencyConverterViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
+enum CurrencyType: String {
+	case cad = "Currency (CAD)"
+	case mxn = "Currency (MXN)"
+}
+
 class CurrencyConverterViewController: UIViewController {
 
+	// MARK: - Outlets and Properties
+	@IBOutlet weak var usdCurrencyTextField: UITextField!
+	@IBOutlet weak var otherCurrencyTextField: UITextField!
+	@IBOutlet weak var toCurrencyLabel: UILabel!
+	@IBOutlet weak var convertButton: UIButton!
+	@IBOutlet weak var CADButton: UIButton!
+	@IBOutlet weak var MXNButton: UIButton!
+	
+	var currencyType: CurrencyType = .cad
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		CADButton.isSelected = true
+		toCurrencyLabel.text = CurrencyType.cad.rawValue
 	}
+	
+	// MARK: - Actions
+	@IBAction func convertButtonTapped(_ sender: UIButton) {
+		
+	}
+	@IBAction func CADButtonSelected(_ sender: UIButton) {
+		CADButton.isSelected = true
+		MXNButton.isSelected = false
+		toCurrencyLabel.text = CurrencyType.cad.rawValue
+	}
+	@IBAction func MXNButtonSelected(_ sender: UIButton) {
+		MXNButton.isSelected = true
+		CADButton.isSelected = false
+		toCurrencyLabel.text = CurrencyType.mxn.rawValue
+	}
+	
 
+	// MARK: - Helper Functions
+	//$1 USD to $1.31 CAD
+	//$1 USD to $19.17 MXN
 
+	func convert(dollars: Double, to unit: CurrencyType) -> Double {
+		var product: Double
+		switch unit {
+		case .cad:
+			product = dollars * 1.31
+		case .mxn:
+			product = dollars * 19.17
+		}
+		return product
+	}
 }
 
